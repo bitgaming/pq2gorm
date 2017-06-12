adfdb2b4a64e7d0f3b7a991f4872eb0e7cc35628
marvin5064 <marvin5064@gmail.com>
2017-06-12 17:15:11 +0800

    rename json and jsonb to string

--- generate.go
+++ generate.go
@@ -216,7 +216,7 @@ func gormDataType(s string) string {
 		return "uint"
 	case "numeric":
 		return "float64"
-	case "character varying", "text", "json":
+	case "character varying", "text", "json", "jsonb":
 		return "string"
 	case "boolean":
 		return "bool"
